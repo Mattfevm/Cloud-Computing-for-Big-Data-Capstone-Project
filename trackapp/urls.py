a6fd9a23e25338d24e02ff8da4d30900bb7986a7
Mattfevm <matias.fevm@gmail.com>
2023-02-08 15:12:55 -0500

    update 2

--- trackapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('track/',views.trackPage,name='track'),
-    path('login/',views.userLogin,name='login'),
-    path('logout',views.userlogout,name='logout'),
-]
